42e37a54d277dac69afe8bd7290db43b0556d007
Lunchbox00711 <hoopa@yahoo.com>
2020-11-20 11:58:32 -0500

    Add files via upload

---
+++ slotted_draft_change_time.php
@@ -0,0 +1,23 @@
+<?
+/***************************************************************************
+ *                                edit_pick.php
+ *                            -------------------
+ *   begin                : Friday, Mar 28, 2008
+ *   copyright            : (C) 2008 J. David Baker
+ *   email                : me@jdavidbaker.com
+ *
+ ***************************************************************************/
+
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+
+include "includes/classes.inc.php";
+$page = new page("slotted_draft_change_time");
+echo $page->draw();
+?>
